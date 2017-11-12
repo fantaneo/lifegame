3af1ccd9098f27961150d48123a6b11e9912e54d
ryuji-i <fantaneo.orange@gmail.com>
2017-11-12 12:02:14 +0900

    クラス化

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -1,5 +1,14 @@
 require "spec_helper"
-require_relative '../src/sample'
+require_relative '../src/lifegame.rb'
 
 RSpec.describe "LifeGame" do
+  it "ブリンカーが出力されること" do
+    lifegame = LifeGame.new
+    lines = []
+    lifegame.output do |line|
+      lines << line + "\n"
+    end
+    blinker = File.read("snapshot")
+    expect(lines.join("")).to eq blinker
+  end
 end
